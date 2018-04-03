6c62bc81a024b368452d91545fce94322239217a
MICHELLE LALLY - STUDENT <michellelallyit1gti@gmail.com>
2018-04-03 15:57:58 +0100

    decryption works but no idea how

--- FourSquareCipher.java
+++ FourSquareCipher.java
@@ -10,39 +10,41 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class FourSquareCipher {
-	static char letterOneEnc, letterTwoEnc, letterOneDec, letterTwoDec; 
-	
+	static char letterOneEnc, letterTwoEnc; 
 	static int letterOneRow, letterOneColumn, letterTwoRow, letterTwoColumn;
+	char let1, let2;
 
-	static char[]alphabet1D =  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', ' ', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char[]alphabet1D =  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', ' ', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-	private char[][] alphabet =  {
-			{'A', 'B', 'C', 'D', 'E'},
-			{'F', 'G', 'H', 'I', 'K'},
-			{'L', 'M', 'N', 'O', 'P'},
-			{' ', 'R', 'S', 'T', 'U'},
-			{'V', 'W', 'X', 'Y', 'Z'}
+	
+	char[][] alphabet =  {
+		{'A', 'B', 'C', 'D', 'E'},
+		{'F', 'G', 'H', 'I', 'K'},
+		{'L', 'M', 'N', 'O', 'P'},
+		{' ', 'R', 'S', 'T', 'U'},
+		{'V', 'W', 'X', 'Y', 'Z'}
 	};	
 	
-	static char[][] keyOne =  {
-			{'Z', 'G', 'P', 'T', 'F'},
-			{'O', 'I', 'H', 'M', 'U'},
-			{'W', 'D', 'R', 'C', 'N'},
-			{'Y', 'K', 'E', 'Q', 'A'},
-			{'X', 'V', 'S', 'B', 'L'}
+	char[][] keyOne =  {
+		{'Z', 'G', 'P', 'T', 'F'},
+		{'O', 'I', 'H', 'M', 'U'},
+		{'W', 'D', 'R', 'C', 'N'},
+		{'Y', 'K', 'E', 'Q', 'A'},
+		{'X', 'V', 'S', 'B', 'L'}
 	};
 	
-	static char[][] keyTwo = {
-		{'M', 'F', 'N', 'B', 'D',},
+	char[][] keyTwo = {
+		{'M', 'F', 'N', 'B', 'D'},
 		{'C', 'R', 'H', 'S', 'A'},
 		{'X', 'Y', 'O', 'G', 'V'},
 		{'I', 'T', 'U', 'E', 'W'},
 		{'L', 'Q', 'Z', 'K', 'P'}
 	};
 	
-	public static void getIndexesLetterOne(char let1) {
-		for(int i = 0; i < alphabet1D.length; i++) {
-			if (alphabet1D[i] == let1) {
+	
+	public static void getIndexesLetterOne(char[] array, char let1) {
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] == let1) {
 				letterOneRow = i / 5;
 				letterOneColumn = i % 5;
 			}
@@ -50,65 +52,72 @@ public class FourSquareCipher {
 	}
 
 	
-	public static void getIndexesLetterTwo(char let2) {
-		for(int i = 0; i < alphabet1D.length; i++) {
-			if (alphabet1D[i] == let2) {
+	public static void getIndexesLetterTwo(char[] array, char let2) {
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] == let2) {
 				letterTwoRow = i / 5;
 				letterTwoColumn = i % 5;
 			}
 		}
 	}
 
-	public static void encrypt(char let1, char let2) {
-		getIndexesLetterOne(let1);
-		getIndexesLetterTwo(let2);
-		letterOneEnc = keyOne[letterOneRow][letterTwoColumn];
-		letterTwoEnc = keyTwo[letterTwoRow][letterOneColumn];
-		String bigram = letterOneEnc+""+letterTwoEnc;
-		System.out.print(bigram);
-	/*			
-		System.out.println("\nA Row: " + letterOneRow);
+	public static void encrypt(char array[], char let1, char let2, char key1[][], char key2[][]) {
+		getIndexesLetterOne(array, let1);
+		getIndexesLetterTwo(array, let2);
+		letterOneEnc = key1[letterOneRow][letterTwoColumn];
+		letterTwoEnc = key2[letterTwoRow][letterOneColumn];
+	/*	System.out.println("\nA Row: " + letterOneRow);
 		System.out.println("A Column: " + letterOneColumn);
 		System.out.println("B Row: " + letterTwoRow);
-		System.out.println("B Column: " + letterTwoColumn);
-	*/ 
+		System.out.println("B Column: " + letterTwoColumn); */
+	//	String bigram = letterOneEnc+""+letterTwoEnc;
+	//	System.out.print(bigram);
 	}
 	
 	//----------------------------------------------------------------------------------------------------------
 	//---------------------------------------------DECRYPTION---------------------------------------------------
 	//----------------------------------------------------------------------------------------------------------
 	
-	public static void getIndexesEncLetterOne(char let1) {
-		for(int i = 0; i < alphabet1D.length; i++) {
-			if (alphabet1D[i] == let1) {
-				letterOneRow = i / 5;
-				letterOneColumn = i % 5;
+	static char letterOneDec, letterTwoDec;
+	static int letterOneEncRow, letterOneEncColumn, letterEncTwoRow, letterEncTwoColumn;
+	
+	public static void getIndexesEncLetterOne(char[][] alpha) {
+		letterOneDec = alpha[letterOneRow][letterOneColumn];
+	/*	
+		for(int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (array[i][j] == let1) {
+					letterOneEncRow = i / 5;
+					letterOneEncColumn = i % 5;
+				}
 			}
-		}
+		}*/
 	}
 
 	
-	public static void getIndexesEncLetterTwo(char let2) {
-		for(int i = 0; i < alphabet1D.length; i++) {
-			if (alphabet1D[i] == let2) {
-				letterTwoRow = i / 5;
-				letterTwoColumn = i % 5;
+	public static void getIndexesEncLetterTwo(char[][] alpha) {
+		letterTwoDec =  alpha[letterTwoRow][letterTwoColumn];
+		
+	/*	for(int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (array[i][j] == let2) {
+					letterOneEncRow = i / 5;
+					letterOneEncColumn = i % 5;
+				}
 			}
-		}
+		}*/
 	}
 
-	public static void decrypt(char let1, char let2) {
-		getIndexesEncLetterOne(let1);
-		getIndexesEncLetterTwo(let2);
-		letterOneEnc = keyOne[letterOneRow][letterTwoColumn];
-		letterTwoEnc = keyTwo[letterTwoRow][letterOneColumn];
-		System.out.print(letterOneEnc+""+letterTwoEnc);
+	public static void decrypt(char[][] alphabet) {
+		getIndexesEncLetterOne(alphabet);
+		getIndexesEncLetterTwo(alphabet);
+		System.out.print(letterOneDec+""+letterTwoDec);
+	/*	letterOneDec = alphabet[letterOneRow][letterTwoColumn];
+		letterTwoDec = keyTwo[letterTwoRow][letterOneColumn];
+		
 
 	/*			
-		System.out.println("\nA Row: " + letterOneRow);
-		System.out.println("A Column: " + letterOneColumn);
-		System.out.println("B Row: " + letterTwoRow);
-		System.out.println("B Column: " + letterTwoColumn);
+
 	*/ 
 	}
 	
--- Parser.java
+++ Parser.java
@@ -3,7 +3,7 @@ package ie.gmit.sw;
 import java.io.*;
 import java.util.*;
 
-public class Parser {
+public class Parser extends FourSquareCipher{
 	
 	public Parser(String fileName) {
 		this.parse(fileName);
@@ -13,23 +13,29 @@ public class Parser {
 		try {
 			//Declaring objects and variables
 			BufferedReader br = new BufferedReader(new FileReader(file));
+		//	ArrayDeque<String> ad = new ArrayDeque<String>();
 			String line;
 			int i;
 			char charA, charB;
+		//	String bigram;
 			
 			//Reading in each line of the file 
 			while ((line = br.readLine()) != null) {
 				//Replacing each j with an i, each q with an o
 				line = line.toUpperCase().replaceAll("[^A-Z]", " ").replace('J', 'I').replace("Q", "O"); 
 				if (line.length() % 2 != 0) {
-					line += "";
+					line += " ";
 				}
 				for (i = 0; i < line.length(); i+=2) {
-						charA = line.charAt(i); 
-						charB  = line.charAt(i+1);
-						FourSquareCipher.encrypt(charA, charB);
+					charA = line.charAt(i); 
+					charB  = line.charAt(i+1);
+					FourSquareCipher.encrypt(alphabet1D, charA, charB, keyOne, keyTwo);
+					FourSquareCipher.decrypt(alphabet);
+					//String[] cipher = bigram.split("\\s+");
+					//ad.addAll(Arrays.asList(cipher));
 				}
 			}
+			br.close();	
 		} catch (IOException e) {
 		e.printStackTrace();
 		}
